04fed03b8a17915674a2a10c2affa7c0ea595fd2
Aljamil1113 <aarazasaljamil@gmail.com>
2022-02-27 17:33:39 +0800

    Using Service w/ CRUD, implement get List items and Delete

--- live-posts/src/app/post-list/post-list.component.ts
+++ live-posts/src/app/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,26 +9,11 @@ import { Post } from '../post.model';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  listOfPosts: Post[] = [
-    new Post(
-      ' Nature',
-   'Ural mountains is a long and wide range mountains on Russsia.',
-   'https://peakvisor.com/img/news/Ural-mountains-South-Ural-Ridge.jpg',
-    'test@test.com',
-    new Date()
-    ),
-    new Post(
-      'Nature',
-   'Yakutsk: The Coldest City in the World',
-   'https://lh3.googleusercontent.com/-i5yQ0exbR4U/WwA2Eqo4_QI/AAAAAAABW-c/RU6ApAdHRMYYhPpRQAZS9VzfV_-9pgL5wCHMYCw/yakutsk-16?imgmax=1600',
-    'test@test.com',
-    new Date()
-    )
+  listOfPosts: Post[] = [];
 
-  ];
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.listOfPosts = this.postService.getPosts();
   }
-
 }
---
+++ live-posts/src/app/post.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+    listOfPosts: Post[] = [
+        new Post(
+          ' Nature',
+       'Ural mountains is a long and wide range mountains on Russsia.',
+       'https://peakvisor.com/img/news/Ural-mountains-South-Ural-Ridge.jpg',
+        'test@test.com',
+        new Date()
+        ),
+        new Post(
+          'Nature',
+       'Yakutsk: The Coldest City in the World',
+       'https://lh3.googleusercontent.com/-i5yQ0exbR4U/WwA2Eqo4_QI/AAAAAAABW-c/RU6ApAdHRMYYhPpRQAZS9VzfV_-9pgL5wCHMYCw/yakutsk-16?imgmax=1600',
+        'test@test.com',
+        new Date()
+        )
+    
+      ];
+
+      getPosts(){
+        return this.listOfPosts;
+      }
+
+      deletePost(index: number) {
+        this.listOfPosts.splice(index, 1);
+      }
+
+      addPost(post: Post){
+        this.listOfPosts.push(post);
+      }
+
+      updatePost(index: number, post: Post){
+        this.listOfPosts[index] = post;
+      }
+}
